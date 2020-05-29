f8c742f9320d70a7515122012a9166bc496e546e
mahrr <ahmadmahrr@protonmail.com>
2020-05-29 07:22:04 +0200

    Exercise 1-38

---
+++ 1.Building-Abstractions-with-Procedures/1-38.scm
@@ -0,0 +1,24 @@
+;; Exercise 1-38
+
+;; Euler's De Fractionibus Continuis
+
+(define (cont-frac n d k)
+  (define (iter i result)
+    (if (= i 0)
+        result
+        (iter (- i 1)
+              (/ (n i) (+ (d i) result)))))
+  (iter k 0))
+
+(define (divisible? x y)
+  (= (remainder x y) 0))
+
+(define (calc-e k)
+  (define n (lambda (x) 1))
+  ;; To generate sequence: 1 2 1 1 4 1 1 6 1 1 8 ...
+  (define d (lambda (x)
+              (let ((i (+ x 1.0)))
+                (if (divisible? i 3.0)
+                    (- i (/ i 3.0))
+                    1.0))))
+  (+ (cont-frac n d k) 2))
